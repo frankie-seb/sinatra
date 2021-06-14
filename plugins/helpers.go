015dc8054c4026a4a6f7147792e5e6f1edaa5b69
Sebastian Poole <seb@frankie.health>
2021-06-14 10:48:05 +1000

    🐛 FIX: template root

--- plugins/helpers.go
+++ plugins/helpers.go
@@ -386,14 +386,7 @@ func enumsWithout(enums []*Enum, skip []string) []*Enum {
 func getTemplateContent(filename string) (string, error) {
 	// load path relative to calling source file
 	_, callerFile, _, _ := runtime.Caller(0) //nolint:dogsled
-	ex, err := os.Executable()
-	if err != nil {
-		panic(err)
-	}
-	exPath := filepath.Dir(ex)
-	fmt.Println(exPath, path.Join(exPath, "internal/templates", filename))
-
-	rootDir := filepath.Dir(callerFile)
+	rootDir := filepath.Dir("../" + callerFile)
 	content, err := ioutil.ReadFile(path.Join(rootDir, "internal/templates", filename))
 	if err != nil {
 		return "", fmt.Errorf("could not read template file: %v", err)
